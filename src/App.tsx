bebeb929be52b373de6563a2a9353367421493e4
jamie.yee <jessie1695@gmail.com>
2026-05-24 02:06:36 +0800

    Add sidebar, logo and update iconMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { AppRoutes } from "./routes/AppRoutes"
+import { AppRoutes } from "./routes/AppRoutes";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "./components/layouts/AppSidebar";
 
 function App() {
-
   return (
-    <>
-      <AppRoutes />
-    </>
-  )
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full">
+        <AppSidebar />
+        <main className="flex-1 p-4">
+          <SidebarTrigger />
+          <AppRoutes />
+        </main>
+      </div>
+    </SidebarProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/layouts/AppSidebar.tsx
@@ -0,0 +1,90 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  User2,
+  LogOut,
+  LayoutDashboard,
+  ArrowRightLeftIcon,
+  Landmark,
+  Bell,
+  Settings,
+} from "lucide-react";
+
+import logo from "../../assets/TorchEye_Ledger_logo.png";
+
+const menuItems = [
+  {
+    label: "Dashboard",
+    icon: <LayoutDashboard />,
+    url: "/",
+  },
+  {
+    label: "Profile",
+    icon: <User2 />,
+    url: "/profile",
+  },
+  {
+    label: "Transactions",
+    icon: <ArrowRightLeftIcon />,
+    url: "/transactions",
+  },
+  {
+    label: "Accounts",
+    icon: <Landmark />,
+    url: "/accounts",
+  },
+  {
+    label: "Notifications",
+    icon: <Bell />,
+    url: "/notifications",
+  },
+  {
+    label: "Settings",
+    icon: <Settings />,
+    url: "/settings",
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <img src={logo}alt="TorchEye Ledger Logo" />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            {menuItems.map((item, index) => (
+              <SidebarMenuItem key={index}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    {item.icon}
+                    {item.label}
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <LogOut /> Logout
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
