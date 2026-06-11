387aca4f462a53a41427343229e2752d2974085a
jamie.yee <jessie1695@gmail.com>
2026-06-11 17:18:25 +0800

    Add default avatar png and profile ui

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Card, CardTitle, CardContent } from "@/components/ui/card";
+import DefaultAvatar from "../assets/default_avatar.png";
 
 export const Profile = () => {
   return (
-    <div>Profile
-      
+    <div>
+      <Card className="border border-amber-200">
+        <CardContent className="flex">
+          <img src={DefaultAvatar} alt="Default Avatar" className="w-40 h-40" />
+          <div className="ml-5">
+            <p>Name: John Doe</p>
+            <p>Email: johndoe@example.com</p>
+          </div>
+        </CardContent>
+      </Card>
     </div>
-  )
-}
+  );
+};
