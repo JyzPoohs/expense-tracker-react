6e4e8fe9d58b41e1d48740a7aa8a76ad775fe732
jamie.yee <jessie1695@gmail.com>
2026-06-16 16:51:48 +0800

    Import sonner and update profile with user auth info

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,15 +1,18 @@
 import { Card, CardTitle, CardContent } from "@/components/ui/card";
 import DefaultAvatar from "../assets/default_avatar.png";
+import { useAuth } from "@/auth/AuthProvider";
 
 export const Profile = () => {
+  const { user } = useAuth();
+
   return (
     <div>
       <Card className="border border-amber-200">
         <CardContent className="flex">
           <img src={DefaultAvatar} alt="Default Avatar" className="w-40 h-40" />
           <div className="ml-5">
-            <p>Name: John Doe</p>
-            <p>Email: johndoe@example.com</p>
+            <p>Name: {user?.username}</p>
+            <p>Email: {user?.email}</p>
           </div>
         </CardContent>
       </Card>
