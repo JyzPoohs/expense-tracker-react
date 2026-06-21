33987d8de05baff9101fa45342beb998be89192d
jamie.yee <jessie1695@gmail.com>
2026-06-21 22:26:17 +0800

    Update categories settings UI

--- src/pages/settings/CategoriesSettings.tsx
+++ src/pages/settings/CategoriesSettings.tsx
@@ -3,6 +3,7 @@ import type { Category } from "@/types/category";
 import { iconMap } from "@/utils/iconMapper";
 import { Badge } from "@/components/ui/badge";
 import { useEffect, useState } from "react";
+import { Label } from "@/components/ui/label";
 
 export const CategoriesSettings = () => {
   const [categories, setCategories] = useState<Category[]>([]);
@@ -17,26 +18,56 @@ export const CategoriesSettings = () => {
   }, []);
   return (
     <div>
-      {categories && categories.length > 0 && (
-        <div className="flex justify-start flex-wrap gap-2 my-3">
-          {categories.map((category: Category, index: number) => {
-            const IconComponent = iconMap[category.icon];
-            return (
-              <Badge
-                key={index}
-                style={{
-                  backgroundColor: category.color,
-                }}
-                className="text-white flex items-center gap-1 h-7"
-              >
-                {IconComponent && <IconComponent className="w-4 h-4" />}
+      <div>
+        <Label>Expense Categories:</Label>
+        {categories && categories.length > 0 && (
+          <div className="flex justify-start flex-wrap gap-2 my-3">
+            {categories
+              .filter((category) => category.type === "EXPENSE")
+              .map((category: Category, index: number) => {
+                const IconComponent = iconMap[category.icon];
+                return (
+                  <Badge
+                    key={index}
+                    style={{
+                      backgroundColor: category.color,
+                    }}
+                    className="text-white flex items-center gap-1 h-7"
+                  >
+                    {IconComponent && <IconComponent className="w-4 h-4" />}
 
-                {category.name}
-              </Badge>
-            );
-          })}
-        </div>
-      )}
+                    {category.name}
+                  </Badge>
+                );
+              })}
+          </div>
+        )}
+      </div>
+      <div>
+        <Label>Income Categories:</Label>
+        {categories && categories.length > 0 && (
+          <div className="flex justify-start flex-wrap gap-2 my-3">
+            {categories
+              .filter((category) => category.type === "INCOME")
+              .map((category: Category, index: number) => {
+                const IconComponent = iconMap[category.icon];
+                return (
+                  <Badge
+                    key={index}
+                    style={{
+                      backgroundColor: category.color,
+                    }}
+                    className="text-white flex items-center gap-1 h-7"
+                  >
+                    {IconComponent && <IconComponent className="w-4 h-4" />}
+
+                    {category.name}
+                  </Badge>
+                );
+              })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
